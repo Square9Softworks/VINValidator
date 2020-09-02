24c9976d6e97cb0a57b0009ceb449cfdc3e0d668
Brian Banet <bbanet@square-9.com>
2020-09-02 13:02:44 -0400

    Add supporting files for Custom Node interfaces.  Simplify logic to leverage property access.

--- Square9.CA.VinValidate/AssemblyLogic.cs
+++ Square9.CA.VinValidate/AssemblyLogic.cs
@@ -6,43 +6,23 @@ using System.Threading.Tasks;
 
 namespace Square9.CA.VinValidate
 {
-    public class AssemblyLogic
+    public class AssemblyLogic : Square9.CustomNode.CaptureNode
     {
-        public Dictionary<string, string> RunCallAssembly(Dictionary<string, string> Input)
+        public override void Run()
         {
-            Dictionary<string, string> Output = new Dictionary<string, string>();
-            String vinKey = "";
-            String vinStatusKey = "";
-            Boolean vinStatus = false;
+            var vinValue = Process.Properties.GetSingleValue(Settings.GetStringSetting("property"));
+            var vinStatus = Validator.Vin.IsValid(vinValue);
 
-            Console.WriteLine(DateTime.Now.ToString() + "\tSquare 9 Softworks VIN Validation.");
-
-            foreach (String item in Input.Keys)
+            if (!vinStatus)
             {
-                if (!vinStatus && !String.IsNullOrEmpty(Input[item]) && Input[item].ToUpper().StartsWith("VIN:"))
-                {
-                    Console.WriteLine(DateTime.Now.ToString() + "\tSquare 9 Softworks VIN Validation: VIN field found - " + Input[item]);
-                    vinKey = item;
-                    vinStatus = Validator.Vin.IsValid(Input[item].Substring(4));
-                }
-
-                if (vinStatusKey == "" && !String.IsNullOrEmpty(Input[item]) && Input[item].ToUpper().StartsWith("VINSTATUS:"))
-                {
-                    Console.WriteLine(DateTime.Now.ToString() + "\tSquare 9 Softworks VIN Validation: VIN status field found - " + item);
-                    vinStatusKey = item;
-                }
+                LogHistory("VIN Check Failed.  Ensure the VIN data is valid.");
+                SetNextNodeByLinkName("Fail");
             }
-
-            if(vinKey == "")
-                Console.WriteLine(DateTime.Now.ToString() + "\tSquare 9 Softworks VIN Validation: No VIN field found.  Data must be prefaced with the literal value \"VIN:\"");
-
-            if(vinStatusKey=="")
-                Console.WriteLine(DateTime.Now.ToString() + "\tSquare 9 Softworks VIN Validation: No return status field found.  Field must have a value of \"VINSTATUS:\"");
-
-            if (!String.IsNullOrEmpty(vinStatusKey))
-                Output.Add(vinStatusKey, vinStatus.ToString());
-
-            return Output;
+            else
+            {
+                LogHistory("VIN passes with valid checksum.");
+                SetNextNodeByLinkName("Pass");
+            }
         }
     }
 }
